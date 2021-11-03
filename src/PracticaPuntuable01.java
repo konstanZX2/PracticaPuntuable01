dd2ea12d31be0bc93d539fa5f0539c66f095bb31
konstanZX2 <zxgenerationzx@gmail.com>
2021-11-03 19:43:33 +0100

    Bueno, algo es algo

--- src/PracticaPuntuable01.java
+++ src/PracticaPuntuable01.java
@@ -3,20 +3,122 @@ import java.util.Scanner;
 public class PracticaPuntuable01 {
     public static void main(String[] args) {
         Scanner escaner = new Scanner(System.in);
-        int[] array = {2, 7, 4, -5, 11, 5, 20};
+       /* int[] array = {2, 7, 4, -5, 11, 5, 20};
         array = new int[7];
         int n = 15;
-        System.out.println("Introduzca un numero");
-        for (int i = array[2]; i =  ; i++) {
-
-        }
-
+        */ //No se hacer arrays aun
+        int numero1;
+        int numero2;
+        int numero3;
+        int numero4;
+        int numero5;
+        int numero6;
+        int numero7;
+        int numeroRequerido;
+        System.out.println("Introduzca un numero que necesites");
+        numeroRequerido = escaner.nextInt();
+        System.out.println("Introduzca numero");
+        numero1 = escaner.nextInt();
+        System.out.println("Introduzca numero");
+        numero2 = escaner.nextInt();
+        System.out.println("Introduzca numero");
+        numero3 = escaner.nextInt();
+        System.out.println("Introduzca numero");
+        numero4 = escaner.nextInt();
+        System.out.println("Introduzca numero");
+        numero5 = escaner.nextInt();
+        System.out.println("Introduzca numero");
+        numero6 = escaner.nextInt();
+        System.out.println("Introduzca numero");
+        numero7 = escaner.nextInt();
+        if (numero1 + numero2 == numeroRequerido) {
+            System.out.println(numero1 + "+" + numero2);
+        } else if (numero1 + numero3 == numeroRequerido) {
+            System.out.println(numero1 + "+" + numero3);
+        } else if (numero1 + numero4 == numeroRequerido) {
+            System.out.println(numero1 + "+" + numero4);
+        } else if (numero1 + numero5 == numeroRequerido) {
+            System.out.println(numero1 + "+" + numero5);
+        } else if (numero1 + numero6 == numeroRequerido) {
+            System.out.println(numero1 + "+" + numero6);
+        } else if (numero1 + numero7 == numeroRequerido) {
+            System.out.println(numero1 + "+" + numero7);
+        } else if (numero2 + numero1 == numeroRequerido) {
+            System.out.println(numero2 + "+" + numero1);
+        } else if (numero2 + numero3 == numeroRequerido) {
+            System.out.println(numero2 + "+" + numero3);
+        } else if (numero2 + numero4 == numeroRequerido) {
+            System.out.println(numero2 + "+" + numero4);
+        } else if (numero2 + numero5 == numeroRequerido) {
+            System.out.println(numero2 + "+" + numero5);
+        } else if (numero2 + numero6 == numeroRequerido) {
+            System.out.println(numero2 + "+" + numero6);
+        } else if (numero2 + numero7 == numeroRequerido) {
+            System.out.println(numero2 + "+" + numero7);
+        } else if (numero3 + numero1 == numeroRequerido) {
+            System.out.println(numero3 + "+" + numero1);
+        } else if (numero3 + numero2 == numeroRequerido) {
+            System.out.println(numero2 + "+" + numero3);
+        } else if (numero3 + numero4 == numeroRequerido) {
+            System.out.println(numero3 + "+" + numero4);
+        } else if (numero3 + numero5 == numeroRequerido) {
+            System.out.println(numero3 + "+" + numero5);
+        } else if (numero3 + numero6 == numeroRequerido) {
+            System.out.println(numero3 + "+" + numero6);
+        } else if (numero3 + numero7 == numeroRequerido) {
+            System.out.println(numero3 + "+" + numero7);
+        } else if (numero4 + numero1 == numeroRequerido) {
+            System.out.println(numero4 + "+" + numero1);
+        } else if (numero4 + numero2 == numeroRequerido) {
+            System.out.println(numero4 + "+" + numero2);
+        } else if (numero4 + numero4 == numeroRequerido) {
+            System.out.println(numero4 + "+" + numero3);
+        } else if (numero4 + numero5 == numeroRequerido) {
+            System.out.println(numero4 + "+" + numero5);
+        } else if (numero4 + numero6 == numeroRequerido) {
+            System.out.println(numero4 + "+" + numero6);
+        } else if (numero4 + numero7 == numeroRequerido) {
+            System.out.println(numero4 + "+" + numero7);
+        } else if (numero5 + numero1 == numeroRequerido) {
+            System.out.println(numero5 + "+" + numero1);
+        } else if (numero5 + numero2 == numeroRequerido) {
+            System.out.println(numero5 + "+" + numero2);
+        } else if (numero5 + numero3 == numeroRequerido) {
+            System.out.println(numero5 + "+" + numero3);
+        } else if (numero4 + numero5 == numeroRequerido) {
+            System.out.println(numero4 + "+" + numero5);
+        } else if (numero5 + numero6 == numeroRequerido) {
+            System.out.println(numero5 + "+" + numero6);
+        } else if (numero5 + numero7 == numeroRequerido) {
+            System.out.println(numero5 + "+" + numero7);
+        } else if (numero6 + numero1 == numeroRequerido) {
+            System.out.println(numero6 + "+" + numero1);
+        } else if (numero6 + numero2 == numeroRequerido) {
+            System.out.println(numero6 + "+" + numero2);
+        } else if (numero6 + numero3 == numeroRequerido) {
+            System.out.println(numero6 + "+" + numero3);
+        } else if (numero6 + numero4 == numeroRequerido) {
+            System.out.println(numero6 + "+" + numero4);
+        } else if (numero6 + numero5 == numeroRequerido) {
+            System.out.println(numero5 + "+" + numero6);
+        } else if (numero6 + numero7 == numeroRequerido) {
+            System.out.println(numero6 + "+" + numero7);
+        } else if (numero7 + numero1 == numeroRequerido) {
+            System.out.println(numero7 + "+" + numero1);
+        } else if (numero7 + numero2 == numeroRequerido) {
+            System.out.println(numero7 + "+" + numero2);
+        } else if (numero7 + numero3 == numeroRequerido) {
+            System.out.println(numero7 + "+" + numero3);
+        } else if (numero7 + numero4 == numeroRequerido) {
+            System.out.println(numero7 + "+" + numero4);
+        } else if (numero7 + numero5 == numeroRequerido) {
+            System.out.println(numero7 + "+" + numero6);
+        } else if (numero7 + numero6 == numeroRequerido) {
+            System.out.println(numero6 + "+" + numero7);
+        } //sin arrays funciona hasta 7 numeros como en el ejemplo.
 
 
     }
 
-
 }
-
-
 
