0ed0a3e1ec0a08aba2023c138c7f3a306ac9eb28
konstanZX2 <zxgenerationzx@gmail.com>
2021-11-03 18:50:33 +0100

    Initial commit

---
+++ src/PracticaPuntuable01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PracticaPuntuable01 {
+    public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
+        int[] array = {2, 7, 4, -5, 11, 5, 20};
+        array = new int[7];
+        int n = 15;
+        System.out.println("Introduzca un numero");
+        for (int i = array[2]; i =  ; i++) {
+
+        }
+
+
+
+    }
+
+
+}
+
+
+
